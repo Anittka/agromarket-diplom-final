bcb4352412aec0d389173af18a76f710b433bae4
Anittka <anikolaevna015@gmail.com>
2026-05-22 16:55:54 +0300

    Update settings.py

--- agromarket/settings.py
+++ agromarket/settings.py
@@ -129,9 +129,10 @@ TEMPLATES = [
 
 # Render автоматически передаёт DATABASE_URL
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # =========================================================
